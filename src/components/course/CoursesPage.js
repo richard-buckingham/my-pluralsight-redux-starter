50fd7a059d7ae90abf05e854750c97c096a5874f
richie.buckingham <richie.buckingham@gmail.com>
2019-01-09 08:25:45 +1100

    addcourse form

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,36 @@
 import React, { Component, PropTypes } from "react";
 
 class CoursesPage extends Component {
+  constructor(props, context) {
+    super(props, context);
+    console.log("in CoursesPage constructor");
+    this.state = {
+      course: { title: "" }
+    };
+  }
+
+  onTitleChange(event) {
+    // make a local copy of component state
+    const course = this.state.course;
+    // update the local copy of state to the latest value
+    course.title = event.target.value;
+    // update component state
+    this.setState({ course: course });
+  }
+
   render() {
     return (
       <div>
-        <h1>Courses</h1>
+        <h1>Courses....</h1>
+        <h2>Add Course</h2>
+
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}
+        />
+
+        <input type="submit" onClick={this.onClickSave} value="save" />
       </div>
     );
   }
