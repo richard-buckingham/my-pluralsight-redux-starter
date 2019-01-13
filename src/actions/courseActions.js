555361538d0a4244035ecb133ba4ba5a94abcb47
richie.buckingham <richie.buckingham@gmail.com>
2019-01-14 08:07:06 +1100

    use action constants

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,8 +1,11 @@
-// define action creators
+// import action constants
+import * as types from "./actionTypes";
+
+//define action creators
 export function createCourse(course) {
   console.log("creating the CREATE_COURSE action");
   return {
-    type: "CREATE_COURSE",
+    type: types.CREATE_COURSE,
     course
   };
 }
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,9 @@
+// import action constants
+import * as types from "../actions/actionTypes";
+
 export default function courseReducer(state = [], action) {
   switch (action.type) {
-    case "CREATE_COURSE":
+    case types.CREATE_COURSE:
       console.log("handlig the CREATE_COURSE action in the reducer");
       return [...state, Object.assign({}, action.course)];
 
