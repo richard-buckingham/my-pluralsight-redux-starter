6608a0d876a250a9e4c7bec3f6ad672c8b3eae7f
richie.buckingham <richie.buckingham@gmail.com>
2019-01-13 18:19:20 +1100

    configure the redux provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import "babel-polyfill";
 import React from "react";
+import { Provider } from "react-redux";
 import { render } from "react-dom";
 import { Router, browserHistory } from "react-router";
 import routes from "./routes";
+
+// configure the redux store
+import configureStore from "./store/configureStore";
+const store = configureStore();
+
+// css imports
 import "./styles/styles.css"; // Webpack can import CSS files too!
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById("app")
 );
